8f342c22e0f33e854fbb720f8a67db90cd26cfb1
Ravindra Singh <51900358+ravindrasinghh@users.noreply.github.com>
2022-02-20 18:39:33 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,10 +2,10 @@ variable "instance_map" {
   description = "A map of key/value to the instance"
   type        = map(string)
   default = {
-    ami           = "ami-0ef669c57b73af73b"
+    ami           = "ENTER AMI"
     instance_type = "t3.medium"
     key_name      = "vm-mig"
-    subnet_id     = "subnet-0f272dad513a7fffd"
+    subnet_id     = "ENTER SUBNET"
 
   }
 }
